4904ffd8250618792a174e8961d5065d6fafdf38
Iustin Ghergu <iusting20@gmail.com>
2022-08-24 12:26:37 +0100

    modified app: tested binding capabilities

--- src/app/hello-world/hello-world.component.ts
+++ src/app/hello-world/hello-world.component.ts
@@ -3,13 +3,19 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-hello-world',
   templateUrl: './hello-world.component.html',
-  styleUrls: ['./hello-world.component.css']
+  styleUrls: ['./hello-world.component.css'],
 })
 export class HelloWorldComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  message = 'THE MESSAGE IS THIS.';
+  devName = 'Iustin Ghergu';
+  siteUrl = 'https://impactfultech.com/';
+  dynamicSiteUrl = 'https://impactfultech.com/';
 
-  ngOnInit(): void {
+  sayHello() {
+    alert('Hello, my dear developer, ' + this.devName + ' :D');
   }
 
+  ngOnInit(): void {}
 }
